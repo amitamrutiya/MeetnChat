ee2cdb77dca7026c4e198008768b2aeda87fd78d
Amit Amrutiya <amitamrutiya2210@gmail.com>
2024-03-15 12:46:06 +0530

    refector code

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,57 +1,21 @@
-"use client";
-
-import { useUser } from "@auth0/nextjs-auth0/client";
-import { useEffect, useState } from "react";
-import { ModeToggle } from "@/components/ui/mode-toggle";
-import { loadFull } from "tsparticles";
-import { Engine } from "@tsparticles/engine";
-import { initParticlesEngine } from "@tsparticles/react";
-import ParticlesComponent from "../components/Particles";
-import functions from "./data/functions";
-import { useRouter } from "next/navigation";
-import { v4 } from "uuid";
 import IconCardButton from "@/components/Icon-card-button";
+import { ModeToggle } from "@/components/ui/mode-toggle";
 
 export default function Home() {
-  const router = useRouter();
-  const { user } = useUser();
-  const [init, setInit] = useState(false);
-  useEffect(() => {
-    initParticlesEngine(async (engine: Engine) => {
-      await loadFull(engine);
-    }).then(() => {
-      setInit(true);
-    });
-  }, []);
   return (
     <main className="min-h-screen h-full overflow-auto justify-center p-5">
-      <ParticlesComponent id="tsparticles" done={init} />
       <ModeToggle />
       <div className="flex flex-col items-center absolute w-[95vw]">
         <div
-          className={`text-8xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 cursor-pointer ${
-            init ? "animate-fadeIn" : ""
-          }`}
+          className={`text-8xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 cursor-pointer animate-fadeIn`}
         >
           All in one Website for Meet
         </div>{" "}
-        <div className="mt-[10%] flex flex-wrap justify-center sm:flex-row">
-          {functions.map((f, i) => (
-            <IconCardButton
-              key={i}
-              onClick={() => {
-                const roomId = v4();
-                user
-                  ? router.push(`/room/${roomId}`)
-                  : router.push("/api/auth/login");
-              }}
-              text={f.title}
-              subtext={f.subtitle}
-              description={f.description}
-              icon1={<f.icon1 />}
-              icon2={<f.icon2 />}
-            />
-          ))}
+        <div className="flex justify-evenly">
+          <section className="my-[10%] flex flex-wrap justify-center">
+            <IconCardButton />
+          </section>
+          <section className="w-1 border-r-2 border-gray-200"></section>{" "}
         </div>
       </div>
     </main>
--- frontend/src/components/icon-card-button.tsx
+++ frontend/src/components/icon-card-button.tsx
@@ -1,44 +1,44 @@
+"use client";
+
 import React from "react";
+import functions from "@/app/data/functions";
 import {
   HoverCard,
   HoverCardContent,
   HoverCardTrigger,
 } from "@/components/ui/hover-card";
 import { useUser } from "@auth0/nextjs-auth0/client";
+import Link from "next/link";
+import { v4 } from "uuid";
 
-export interface IconCardButtonProps {
-  icon1: React.ReactNode;
-  icon2: React.ReactNode;
-  text: string;
-  subtext: string;
-  description: string;
-  onClick?: () => void;
-}
-
-const IconCardButton: React.FC<IconCardButtonProps> = (props) => {
-  const { isLoading } = useUser();
-  const { icon1, icon2, text, subtext, description, onClick } = props;
-  return (
-    <HoverCard>
-      <HoverCardTrigger>
-        {" "}
-        <div
-          onClick={onClick}
-          className="text-center mx-5 my-5 px-2 flex h-[250px] w-[250px] cursor-pointer items-center justify-center rounded-md border-2 border-foreground shadow-md hover:bg-hover sm:my-0"
-        >
-          <div className="text-center">
-            <div className="flex justify-center pb-2">
-              <span className="text-primary pr-4">{icon1}</span>
-              <span className="text-primary">{icon2}</span>
+const IconCardButton = () => {
+  const { isLoading, user } = useUser();
+  return functions.map((f, i) => (
+    <Link key={i} href={user ? `/room/${v4()}` : "/api/auth/login"} passHref>
+      <HoverCard>
+        <HoverCardTrigger>
+          {" "}
+          <div className="text-center mx-5 my-5 px-2 flex h-[250px] w-[250px] cursor-pointer items-center justify-center rounded-md border-2 border-foreground shadow-md hover:bg-hover sm:my-0">
+            <div className="text-center">
+              <div className="flex justify-center pb-2">
+                <span className="text-primary pr-4">
+                  {React.createElement(f.icon1)}
+                </span>
+                <span className="text-primary">
+                  {React.createElement(f.icon2)}
+                </span>
+              </div>
+              <p className="font-sans text-lg font-bold text-primary">
+                {f.title}
+              </p>
+              <p className="text-md font-sans font-bold">{f.subtitle}</p>
             </div>
-            <p className="font-sans text-lg font-bold text-primary">{text}</p>
-            <p className="text-md font-sans font-bold">{subtext}</p>
           </div>
-        </div>
-      </HoverCardTrigger>
-      <HoverCardContent>{description}</HoverCardContent>
-    </HoverCard>
-  );
+        </HoverCardTrigger>
+        <HoverCardContent>{f.description}</HoverCardContent>
+      </HoverCard>
+    </Link>
+  ));
 };
 
 export default IconCardButton;
