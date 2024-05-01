5ca9f0ff91dc77f698bc19181486189c93b0603b
amitamrutiya2210 <amitamrutiya2210@gmail.com>
2024-05-01 18:57:59 +0530

    create basic layout
    Signed-off-by: amitamrutiya2210 <amitamrutiya2210@gmail.com>

--- frontend/src/app/room/one-chat/page.tsx
+++ frontend/src/app/room/one-chat/page.tsx
@@ -1,10 +1,9 @@
 export default function room() {
   return (
-    <div className="flex flex-col min-h-screen w-full justify-center items-center">
-      <h1>One to One Chat</h1>
-      <h2>
-        This page still under development. You can explore other functionality.
-      </h2>
+    <div className="min-h-screen flex flex-col md:flex-row gap-4 p-4 overflow-auto">
+      <section className="flex-grow bg-gray-300 rounded-3xl xl:w-1/4 md:w-1/3 md:block hidden"></section>
+      <main className="flex-grow bg-gray-600 rounded-3xl xl:w-1/2 md:w-2/3"></main>
+      <section className="flex-grow bg-gray-900 rounded-3xl xl:w-1/4 md:block hidden"></section>
     </div>
   );
 }
