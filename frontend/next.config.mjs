57e099990a856f52ae860b12db81628fb4074a3e
amitamrutiya2210 <amitamrutiya2210@gmail.com>
2024-05-01 10:35:25 +0530

    add group video call page
    Signed-off-by: amitamrutiya2210 <amitamrutiya2210@gmail.com>

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['localhost', 's.gravatar.com']
+        domains: ['localhost', 's.gravatar.com','lh3.googleusercontent.com']
     }
 };
 
