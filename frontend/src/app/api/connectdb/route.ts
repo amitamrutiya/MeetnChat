90c51d8d902ab5c64c661f58f7387db3d55fcb30
amitamrutiya2210 <amitamrutiya2210@gmail.com>
2024-05-06 19:22:53 +0530

    remove auth0
    Signed-off-by: amitamrutiya2210 <amitamrutiya2210@gmail.com>

--- frontend/src/app/api/connectdb/route.ts
+++
@@ -1,7 +0,0 @@
-import connectDB from "@/config/database";
-import { NextResponse } from "next/server";
-
-export async function GET() {
-  const res = await connectDB();
-  return new NextResponse("connect");
-}
