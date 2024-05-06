bef8e1eb360780b2e05d98acafa7042f06863ccf
amitamrutiya2210 <amitamrutiya2210@gmail.com>
2024-05-06 17:41:23 +0530

    create profile model
    Signed-off-by: amitamrutiya2210 <amitamrutiya2210@gmail.com>

--- frontend/src/model/user.model.ts
+++ frontend/src/model/user.model.ts
@@ -2,26 +2,18 @@ import mongoose, { Document, Schema } from "mongoose";
 import { chatSchema } from "./chat.model";
 
 export interface User extends Document {
-  name: string;
   email: string;
   password: string;
-  profile_image: string;
-  bio: string;
-  phone_number: string;
   username: string;
-  friends: mongoose.Schema.Types.ObjectId[];
-  is_setup: boolean;
-  is_online: boolean;
+  is_online?: boolean;
   is_verified: boolean;
+  verifyCode: string;
+  verifyCodeExpiry: Date;
   chats: mongoose.Types.ObjectId[];
 }
 
 const userSchema: Schema<User> = new Schema(
   {
-    name: {
-      type: String,
-      required: [true, "Name is required"],
-    },
     email: {
       type: String,
       unique: true,
@@ -32,31 +24,9 @@ const userSchema: Schema<User> = new Schema(
       type: String,
       required: [true, "Password is required"],
     },
-    profile_image: {
-      type: String,
-    },
-    bio: {
-      type: String,
-      default: "Hey there! I am available on ChatApp",
-    },
-    phone_number: {
-      type: String,
-      match: [/^\d{10}$/, "Please enter a valid phone number"],
-      unique: true,
-    },
     username: {
       type: String,
       unique: true,
-    },
-    friends: [
-      {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
-      },
-    ],
-    is_setup: {
-      type: Boolean,
-      default: false,
     },
     is_online: {
       type: Boolean,
@@ -66,13 +36,21 @@ const userSchema: Schema<User> = new Schema(
       type: Boolean,
       default: false,
     },
+    verifyCode: {
+      type: String,
+      required: [true, "Verify Code is required"],
+    },
+    verifyCodeExpiry: {
+      type: Date,
+      required: [true, "Verify Code Expiry is required"],
+    },
     chats: [chatSchema],
   },
   { timestamps: true }
 );
 
-const userModel =
+const UserModel =
   (mongoose.models.User as mongoose.Model<User>) ||
   mongoose.model<User>("User", userSchema);
 
-export default userModel;
+export default UserModel;
