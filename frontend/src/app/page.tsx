3ee9aeaa86323c04f7074f2cc83432644176760e
Amit Amrutiya <amitamrutiya2210@gmail.com>
2024-06-13 12:29:25 +0530

    add flip word in headline
    Signed-off-by: Amit Amrutiya <amitamrutiya2210@gmail.com>

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,8 +2,10 @@ import IconCardButton from "@/components/Icon-card-button";
 import JoinRoomForm from "@/components/JoinRoomForm";
 import ParticlesComponent from "@/components/particles";
 import Navbar from "@/components/Navbar";
+import { FlipWords } from "@/components/ui/filp-word";
 
 export default function Home() {
+  const words = ["Omegal", "Google Meet", "Whatsapp"];
   return (
     <main className="h-dvh overflow-auto">
       <ParticlesComponent id="particles" />
@@ -11,9 +13,10 @@ export default function Home() {
 
       <div className="flex flex-col items-center text-center absolute">
         <div
-          className={`min-h-10vh text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 cursor-pointer animate-fadeIn my-5`}
+          className={`min-h-10vh text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 cursor-pointer animate-fadeIn my-5`}
         >
-          All in one Website for Meet
+          Miss <FlipWords words={words} />
+          ? <br /> All in one Website for Meet
         </div>
         <div className="flex flex-col-reverse lg:flex-row min-h-[80ovh]">
           <section className="flex-grow lg:w-2/3 flex flex-wrap justify-center items-center">
