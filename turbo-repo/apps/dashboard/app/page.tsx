d845a537c86968c2c59203397f850ac3b0d4548f
Amit Amrutiya <amitamrutiya2210@gmail.com>
2024-07-01 12:08:33 +0530

    chore: Update dashboard package name and dev server ports

--- turbo-repo/apps/dashboard/app/page.tsx
+++ turbo-repo/apps/dashboard/app/page.tsx
@@ -24,7 +24,7 @@ export default function Home() {
         <div className={styles.ctas}>
           <a
             className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            href="http://localhost:3002"
             target="_blank"
             rel="noopener noreferrer"
           >
@@ -38,7 +38,7 @@ export default function Home() {
             Deploy now
           </a>
           <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            href="http://localhost:3001"
             target="_blank"
             rel="noopener noreferrer"
             className={styles.secondary}
