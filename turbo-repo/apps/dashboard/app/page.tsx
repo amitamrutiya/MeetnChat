47e18dfba408e7039c11d8e65d2d24a52d973a8d
Amit Amrutiya <amitamrutiya2210@gmail.com>
2024-07-01 12:32:00 +0530

    chore: Update Tailwind CSS configuration and styles

--- turbo-repo/apps/dashboard/app/page.tsx
+++ turbo-repo/apps/dashboard/app/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
-import { Button } from "@repo/ui/button";
 import styles from "./page.module.css";
+import { Button } from "@repo/ui";
 
 export default function Home() {
   return (
@@ -47,7 +47,6 @@ export default function Home() {
           </a>
         </div>
         <Button
-          appName="web"
           className="mx-auto rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
         >
           Open alert
--- turbo-repo/apps/dashboard/next.config.mjs
+++ turbo-repo/apps/dashboard/next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const nextConfig = {
+  transpilePackages: ["@repo/ui"],
+};
 
 export default nextConfig;
