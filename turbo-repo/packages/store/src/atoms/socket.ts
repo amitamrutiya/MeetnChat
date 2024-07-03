dc1c43fee6d4da5e7fb10985390384fc3fd7b9da
Amit Amrutiya <amitamrutiya2210@gmail.com>
2024-07-03 16:40:14 +0530

    chore: Update npm dependencies and add new atoms for audio, video, and file transfer
    Signed-off-by: Amit Amrutiya <amitamrutiya2210@gmail.com>

--- turbo-repo/packages/store/src/atoms/socket.ts
+++ turbo-repo/packages/store/src/atoms/socket.ts
@@ -1,8 +1,7 @@
-import { atom } from 'recoil';
-import { Socket } from 'socket.io-client';
+import { atom } from "recoil";
+import { Socket } from "socket.io-client";
 
-// Step 1: Define a Recoil atom for the socket state
 export const socketStateAtom = atom<Socket | null>({
-  key: 'socketStateAtom', // unique ID (with respect to other atoms/selectors)
-  default: null, // default value (aka initial value)
+  key: "socketStateAtom",
+  default: null,
 });
