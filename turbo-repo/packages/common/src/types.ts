ba2bfb69b14f439438ceb34d92e637a53f2e4b03
Amit Amrutiya <amitamrutiya2210@gmail.com>
2024-07-03 16:38:46 +0530

    chore: Update npm dependencies and add new schemas for chat, profile, room, signup, signin, and verify
    Signed-off-by: Amit Amrutiya <amitamrutiya2210@gmail.com>

--- turbo-repo/packages/common/src/types.ts
+++ turbo-repo/packages/common/src/types.ts
@@ -1,3 +1,5 @@
+import { Chat } from "@prisma/client";
+
 export type AvailableFiles = {
   name: string;
   size: number;
@@ -5,5 +7,40 @@ export type AvailableFiles = {
   recievedSize?: number;
   checksum?: string;
   checksumMatched?: boolean;
+  timestamp: number;
+};
+
+export interface ApiResponse {
+  success: boolean;
+  message: string;
+  isAcceptingMessages?: boolean;
+  chats?: Array<Chat>;
+}
+
+export type User = {
+  socketId: string;
+  roomId: string;
+  email?: string;
+  email_verified?: boolean;
+  name?: string;
+  username?: string;
+  nickname?: string;
+  image?: string;
+  sid?: string;
+  joinedAt: Date;
+  isConnected: boolean;
+};
+export type IncomingCall = {
+  roomId: string;
+  from: string;
+  user: User;
+  offer: RTCSessionDescriptionInit;
+};
+
+export type Message = {
+  from: string;
+  displayPicture: string;
+  message: string;
+  isSelf: boolean;
   timestamp: number;
 };
