7409e1cb6cc9710304013874797add080e35c633
Amit Amrutiya <amitamrutiya2210@gmail.com>
2024-07-11 10:26:46 +0530

    chore: Update import paths for components in group-chat page

--- apps/web/app/room/group-chat/page.tsx
+++ apps/web/app/room/group-chat/page.tsx
@@ -1,20 +1,48 @@
 "use client";
 
 import { Button } from "@repo/ui";
+import { useSession } from "next-auth/react";
 import { useEffect, useState } from "react";
 
 export default function Room() {
   const [socket, setSocket] = useState<WebSocket | null>(null);
+  const session = useSession();
+  const user = session.data?.user;
 
   useEffect(() => {
     const newSocket = new WebSocket("ws://localhost:7000");
     newSocket.onopen = () => {
+      const authData = JSON.stringify({
+        type: "AUTH_DATA",
+        payload: {
+          userId: user?.id,
+          name: user?.name,
+        },
+      });
+      newSocket.send(authData);
       console.log("Connection established");
-      newSocket.send("Hello Server!");
     };
-    newSocket.onmessage = (message) => {
-      console.log("Message received:", message.data);
+
+    newSocket.onmessage = (event) => {
+      const message = JSON.parse(event.data);
+      switch (message.type) {
+        case "SEND_MESSAGE":
+          console.log("Message sent:", message.payload.message);
+          //  setMessages((prevMessages) => [...prevMessages, message.message]);
+          break;
+        case "messageUpdated":
+          //  setMessages((prevMessages) =>
+          //    prevMessages.map((msg) => (msg.id === message.message.id ? message.message : msg))
+          //  );
+          break;
+        case "messageDeleted":
+          //  setMessages((prevMessages) => prevMessages.filter((msg) => msg.id !== message.message));
+          break;
+        default:
+          console.error("Unknown message type:", message.type);
+      }
     };
+    
     newSocket.onerror = (error) => {
       console.error("WebSocket error:", error);
     };
@@ -31,7 +59,20 @@ export default function Room() {
     <div className="flex flex-col min-h-screen w-full justify-center items-center">
       <h1>Group Chat</h1>
       <h2>This page is still under development. You can explore other functionality.</h2>
-      <Button onClick={() => socket?.send("Hello!")}>Send Hello Message</Button>
+      <Button
+        onClick={() =>
+          socket?.send(
+            JSON.stringify({
+              type: "SEND_MESSAGE",
+              payload: {
+                message: "Hello, World!",
+              },
+            })
+          )
+        }
+      >
+        Send Hello Message
+      </Button>
     </div>
   );
 }
