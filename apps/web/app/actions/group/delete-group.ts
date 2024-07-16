3e271cd5c89d92a27622031027d16b6520c942e3
Amit Amrutiya <amitamrutiya2210@gmail.com>
2024-07-16 09:12:04 +0530

    feat: Add deleteGroup and getGroups actions for group functionality

---
+++ apps/web/app/actions/group/delete-group.ts
@@ -0,0 +1,25 @@
+"use server";
+
+import db from "@repo/db/client";
+
+type deleteGroupPropsType = {
+  group_id: string;
+};
+
+export const deleteGroup = async ({ group_id }: deleteGroupPropsType) => {
+  if (!group_id) {
+    return { success: false, message: "Invalid data" };
+  }
+
+  const group = await db.group.delete({
+    where: {
+      id: group_id,
+    },
+  });
+
+  if (!group) {
+    return { success: false, message: "Group not deleted" };
+  }
+
+  return { success: true, data: group, message: "Group deleted" };
+};
--- apps/web/app/actions/group/get-groups.ts
+++ apps/web/app/actions/group/get-groups.ts
@@ -1,1 +1,24 @@
-  
+"use server";
+
+import db from "@repo/db/client";
+
+type GetGroupsPropsType = {
+  creator_id: string;
+};
+
+export const getGroups = async ({ creator_id }: GetGroupsPropsType) => {
+  if (!creator_id) {
+    return { success: false, message: "Invalid data" };
+  }
+
+  const groups = await db.group.findMany({
+    where: {
+      creator_id,
+    },
+  });
+
+  if (!groups) {
+    return { success: false, message: "No groups found" };
+  }
+  return { success: true, data: groups, message: "Groups found" };
+};
