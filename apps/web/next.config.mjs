7b2344dbf619e61fa5d4c31f3d0da54a411a5920
Amit Amrutiya <amitamrutiya2210@gmail.com>
2024-08-05 09:28:00 +0530

    chore: Update paths in tsconfig.json and components.json
    Signed-off-by: Amit Amrutiya <amitamrutiya2210@gmail.com>

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["localhost", "s.gravatar.com", "lh3.googleusercontent.com", "github.com", "api.dicebear.com"],
+    domains: [
+      "localhost",
+      "s.gravatar.com",
+      "lh3.googleusercontent.com",
+      "github.com",
+      "api.dicebear.com",
+      "images.unsplash.com",
+    ],
     formats: ["image/avif", "image/webp"],
     loader: "default",
     path: "/_next/image",
